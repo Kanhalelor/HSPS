358d2c3ea5659829cfe16906f959550ba8ae1c8f
Robson <kanhalelor@gmail.com>
2022-07-10 18:12:01 +0200

    updates

---
+++ assets/js/counter.js
@@ -0,0 +1,23 @@
+// create a counter to count the number of items in the school's statistic div
+
+// select elements
+
+const counterEl = document.querySelectorAll('#number');
+console.log(counterEl);
+const values = [];
+for (let i = 0; i < counterEl.length; i+=1) {
+	values.push(parseInt(counterEl[i].innerText));
+}
+console.log(values);
+
+
+
+
+// counter event handler
+const counterFunc = (arr) => {
+	const scrollDiv = document.querySelector('.school-statistics')
+	scrollDiv.addEventListener('scroll', startCounter);
+	if (scrollDiv.offsetHeight + scrollDiv.scrollTop <= scrollDiv.scrollHeight) {  
+	    console.log('scrolled to Top')  
+	  }
+}
---
+++ assets/js/downloads.js
@@ -0,0 +1,9 @@
+
+
+const downloadBts = document.querySelectorAll(".download-item-btn");
+
+// for (let i = downloadBts.length - 1 > i; i+=1) {
+// 	downloadBts.addEventLlistener('click', () => {
+// 		e.target.setAttribute.add("download", "");
+// 	})
+// }
