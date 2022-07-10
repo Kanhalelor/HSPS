8b577c51e496626bc3fbd061f11366304f0ad560
Robson <kanhalelor@gmail.com>
2022-07-10 16:23:19 +0200

    updates

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,51 +1,51 @@
 const responsiveSlider = function () {
-  const slider = document.getElementById("slider");
-  let sliderWidth = slider.offsetWidth;
-  const slideList = document.getElementById("slideWrap");
-  let count = 1;
-  const items = slideList.querySelectorAll("li").length;
-  const prev = document.getElementById("prev");
-  const next = document.getElementById("next");
+const slider = document.getElementById("slider");
+let sliderWidth = slider.offsetWidth;
+const slideList = document.getElementById("slideWrap");
+let count = 1;
+const items = slideList.querySelectorAll("li").length;
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
 
-  window.addEventListener("resize", function () {
-    sliderWidth = slider.offsetWidth;
-  });
+window.addEventListener("resize", () => {
+  sliderWidth = slider.offsetWidth;
+});
 
-  const prevSlide = function () {
-    if (count > 1) {
-      count = count - 2;
-      slideList.style.left = "-" + count * sliderWidth + "px";
-      count++;
-    } else if (count === 1) {
-      count = items - 1;
-      slideList.style.left = "-" + count * sliderWidth + "px";
-      count++;
-    }
-  };
+const prevSlide = () => {
+  if (count > 1) {
+    count = count - 2;
+    slideList.style.left = "-" + count * sliderWidth + "px";
+    count++;
+  } else if (count === 1) {
+    count = items - 1;
+    slideList.style.left = "-" + count * sliderWidth + "px";
+    count++;
+  }
+};
 
-  const nextSlide = function () {
-    if (count < items) {
-      slideList.style.left = "-" + count * sliderWidth + "px";
-      count++;
-    } else if (count === items) {
-      slideList.style.left = "0px";
-      count = 1;
-    }
-  };
+const nextSlide = () => {
+  if (count < items) {
+    slideList.style.left = "-" + count * sliderWidth + "px";
+    count++;
+  } else if (count === items) {
+    slideList.style.left = "0px";
+    count = 1;
+  }
+};
 
-  next.addEventListener("click", function () {
-    nextSlide();
-  });
+next.addEventListener("click", () => {
+  nextSlide();
+});
 
-  prev.addEventListener("click", function () {
-    prevSlide();
-  });
+prev.addEventListener("click", () => {
+  prevSlide();
+});
 
-  setInterval(function () {
-    nextSlide();
+setInterval( () => {
+  nextSlide();
   }, 5000);
 };
 
-window.onload = function () {
+window.onload = () => {
   responsiveSlider();
 };
